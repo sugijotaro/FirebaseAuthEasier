4289a2bfea76a3b2957096b533040dd41ddf1c20
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-05-28 00:20:30 +0900

    Create FirebaseAuthViewModel.swift

---
+++ Sources/FirebaseAuthEasier/FirebaseAuthViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  FirebaseAuthViewModel.swift
+//  FirebaseAuthEasier
+//
+//  Created by Jotaro Sugiyama on 2025/05/27.
+//
+
+import Foundation
+import FirebaseAuth
+import Combine
+import UIKit
+
+@MainActor
+public final class FirebaseAuthViewModel: ObservableObject {
+    @Published public var isSigningIn: Bool = false
+    public let providers: [SignInProviderType]
+    public let labelType: SignInButtonLabelType
+    public let onSignInStart: ((SignInProviderType) -> Void)?
+    public let didSignIn: ((Result<AuthDataResult, Error>) -> Void)?
+    private let authService = FirebaseAuthService()
+    
+    public init(
+        providers: [SignInProviderType]? = nil,
+        labelType: SignInButtonLabelType = .signIn,
+        onSignInStart: ((SignInProviderType) -> Void)? = nil,
+        didSignIn: ((Result<AuthDataResult, Error>) -> Void)? = nil
+    ) {
+        self.providers = providers ?? [.apple, .google]
+        self.labelType = labelType
+        self.onSignInStart = onSignInStart
+        self.didSignIn = didSignIn
+    }
+    
+    public func handleSignIn(provider: SignInProviderType) {
+        onSignInStart?(provider)
+        isSigningIn = true
+        switch provider {
+        case .apple:
+            authService.startSignInWithApple { [weak self] result in
+                guard let self = self else { return }
+                Task { @MainActor in
+                    self.isSigningIn = false
+                    self.didSignIn?(result)
+                }
+            }
+        case .google:
+            guard let rootVC = Self.getRootViewController() else {
+                isSigningIn = false
+                didSignIn?(.failure(NSError(domain: "FirebaseAuthViewModel", code: 0, userInfo: [NSLocalizedDescriptionKey: "RootViewController not found."])))
+                return
+            }
+            authService.startSignInWithGoogle(presentingViewController: rootVC) { [weak self] result in
+                guard let self = self else { return }
+                Task { @MainActor in
+                    self.isSigningIn = false
+                    self.didSignIn?(result)
+                }
+            }
+        }
+    }
+    
+    private static func getRootViewController() -> UIViewController? {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScenes = scenes.first as? UIWindowScene
+        return windowScenes?.keyWindow?.rootViewController
+    }
+}
